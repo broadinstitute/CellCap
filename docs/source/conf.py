5d1632947313c0fef3179f00bf24092da5893301
Yang Xu <35667575+ImXman@users.noreply.github.com>
2023-11-15 10:00:46 -0500

    minor changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,33 +1,11 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-# from cellbender.base_cli import get_version
-
-
-dir_, _ = os.path.split(__file__)
-root_dir = os.path.abspath(os.path.join(dir_, '../../../../../Downloads/CellBender-master/docs', '..'))
-sys.path.insert(0, root_dir)
-
 # -- Project information -----------------------------------------------------
 
 project = 'CellCap'
 copyright = '2023, Data Sciences Platform (DSP), Broad Institute'
 author = 'Yang Xu, Stephen Fleming, and Mehrtash Babadi'
 
-# The full version, including alpha/beta/rc tags
-# version = get_version()
-# release = get_version()
+version = '0.1.0'
+release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 
@@ -67,8 +45,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-# html_favicon = '_static/design/favicon.ico'
+html_static_path = ['_static']
 html_css_files = [
     'theme_overrides.css',  # override wide tables in RTD theme
 ]
